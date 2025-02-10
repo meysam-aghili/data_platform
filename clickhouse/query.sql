10223a64d2a016e6a891988040db8168d6189dee
meysam <meysamaghili533@gmail.com>
2025-02-10 21:00:57 +0330

    test

--- clickhouse/query.sql
+++ clickhouse/query.sql
@@ -1,12 +1,17 @@
--- Active: 1739199614877@@172.20.65.42@8010@default
-CREATE DATABASE test2;
+-- Active: 1739199614877@@172.20.65.42@8122@default
 
-Drop TABLE if EXISTS test.my_table;
-CREATE TABLE IF NOT EXISTS test.my_table on cluster cluster_nesaj(
+SELECT *
+FROM system.clusters
+
+DROP DATABASE IF EXISTS test ON CLUSTER 'cluster_nesaj';
+CREATE DATABASE test ON CLUSTER 'cluster_nesaj';
+
+Drop TABLE if EXISTS test.my_table ON CLUSTER 'cluster_nesaj';
+CREATE TABLE IF NOT EXISTS test.my_table ON CLUSTER 'cluster_nesaj'(
     id UInt32,
     name String,
     timestamp DateTime
-) ENGINE = MergeTree()
+) ENGINE = ReplicatedMergeTree('/clickhouse/tables/{database}/my_table', '{replica}')
 PARTITION BY toYYYYMM(timestamp)
 ORDER BY (id, timestamp)
 SETTINGS storage_policy = 's3_main';
@@ -14,6 +19,7 @@ SETTINGS storage_policy = 's3_main';
 
 INSERT into test.my_table VALUES(1, 'meysam', now())
 
+select * from test.my_table
 
 
 SELECT

9e2f4b7df685b3662b7b168cd35be0104014afa9
meysam <meysamaghili533@gmail.com>
2026-02-08 23:59:41 +0330

    tedstt

--- forgejo/commands.sh
+++ forgejo/commands.sh
@@ -1,2 +1,8 @@
 # run runner in background
+useradd --create-home forgejo-runner
+usermod -aG docker forgejo-runner
+sudo forgejo-runner register
+sudo mv .runner /home/forgejo-runner/
+sudo chown forgejo-runner /home/forgejo-runner/.runner
+sudo su forgejo-runner
 forgejo-runner daemon --config /etc/forgejo-runner-config.yml > /dev/null 2>&1 &

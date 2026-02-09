0ac8354243ce6c8971423e8b6d0dd88bd6e510fb
meysam <meysamaghili533@gmail.com>
2026-02-09 22:26:20 +0330

    deploy registry

--- forgejo/commands.sh
+++ forgejo/commands.sh
@@ -4,5 +4,33 @@ usermod -aG docker forgejo-runner
 sudo forgejo-runner register
 sudo mv .runner /home/forgejo-runner/
 sudo chown forgejo-runner /home/forgejo-runner/.runner
+
+# run one time
 sudo su forgejo-runner
 forgejo-runner daemon --config /etc/forgejo-runner-config.yml > /dev/null 2>&1 &
+
+# or add to services
+sudo nano /etc/systemd/system/forgejo-runner.service
+""
+    [Unit]
+    Description=Forgejo Runner
+    After=network.target
+
+    [Service]
+    Type=forking
+    User=forgejo-runner
+    Group=forgejo-runner
+    WorkingDirectory=/home/forgejo-runner
+    ExecStart=/bin/bash -c "/usr/local/sbin/forgejo-runner daemon --config /etc/forgejo-runner-config.yml > /dev/null 2>&1 &"
+    Restart=on-failure
+    RestartSec=10s
+
+    [Install]
+    WantedBy=multi-user.target
+""
+sudo systemctl daemon-reload
+sudo systemctl enable forgejo-runner.service
+sudo systemctl start forgejo-runner.service
+sudo systemctl status forgejo-runner.service
+sudo journalctl -u forgejo-runner -f
+ps aux | grep forgejo-runner
